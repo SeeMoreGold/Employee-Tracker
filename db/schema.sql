856c571571136ee8cc142e8a8f9b7b006dabd878
SeeMoreGold <moreau.cj@gmail.com>
2020-11-13 17:48:54 -0500

    schema updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE employee_table (
-  id INT PRIMARY KEY,
+  id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
@@ -16,15 +16,16 @@ CREATE TABLE department_table (
   id INT NOT NULL,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (id) REFERENCES employee_table(id)
 );
 
 CREATE TABLE role_table (
   id INT NOT NULL,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10,4) NULL,
-  department_id INT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,4) NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (id) REFERENCES employee_table(id)
 );
 
 -- SELECT * FROM top5000;
--- select * from top_albums;

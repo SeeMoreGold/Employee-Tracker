feb7f40f156eeef5f926acc9b98ab6c216ce5993
SeeMoreGold <moreau.cj@gmail.com>
2020-11-13 18:36:56 -0500

    seed file updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,29 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE employee_table (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role_table(id)
+  -- FOREIGN KEY (manager_id) REFERENCES employee_table(id)
 );
 
 CREATE TABLE department_table (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
-  FOREIGN KEY (id) REFERENCES employee_table(id)
 );
 
 CREATE TABLE role_table (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,4) NOT NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (id)
-  FOREIGN KEY (id) REFERENCES employee_table(id)
+  FOREIGN KEY (department_id) REFERENCES department_table(id)
 );
 
 -- SELECT * FROM top5000;
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee_table (first_name, last_name, role_id)
+VALUES ("Bjorn", "Moore", 3), ("Margo", "Largo", 4), ("Kenneth", "Yauger", 5);
+
+INSERT INTO role_table (title, salary)
+VALUES ("Sales Lead", 80000), ("Sales Person", 55000), ("Lead Engineer", 120000), ("Software Engineer", 90000),
+    ("Account Manager", 100000), ("Accountant", 75000), ("Legal Team Lead", 160000), ("Lawyer", 120000);
+
+INSERT INTO department_table (department_name)
+VALUES ("Sales"), ("Engineering"), ("Accounting"), ("Legal");
